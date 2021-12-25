ffcca43b41d07875f3b34a49dfabcfa59555cd60
EC2 Default User <ec2-user@ip-172-31-19-56.ap-northeast-1.compute.internal>
2021-12-25 04:44:00 +0000

    [Add]編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  resources:layouts
+  # resources:layouts
   # root to:'layouts#top'
-  get 'top' => 'homes#top'
-  get 'todolists/new'
-  post 'todolists' => 'todolists#create'
-  get 'todolists' => 'todolists#index'
-  get 'todolists/:id' => 'todolists#show',as: 'todolist'
+  # get 'top' => 'homes#top'
+  # get 'todolists/new'
+  # post 'todolists' => 'todolists#create'
+  # get 'todolists' => 'todolists#index'
+  # get 'todolists/:id' => 'todolists#show',as: 'todolist'
+  # get 'todolists/:id/edit' => 'todolists#edit',as: 'edit_todolist'
+  # patch 'todolist/:id' => 'todolist#update',as: 'update_todolist'
+  resources :todolists
 end
 
