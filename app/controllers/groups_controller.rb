6999ccd6b4cc67433960a7374e8309ae448aaa10
dai-guu <daiguu2021@gmail.com>
2022-01-15 02:44:08 +0000

    [Add]Group_test1

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -29,10 +29,11 @@ class GroupsController < ApplicationController
   end
 
   def edit
-
+    @group = Group.find(params[:id])
   end
 
   def update
+    @group = Group.find(params[:id])
     if @group.update(group_params)
 
       redirect_to groups_path
